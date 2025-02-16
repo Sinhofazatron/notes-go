50b60bbfd1a20487f15fd9888565f36b79765816
Dmitry Sitnikov <betani.elvin@mail.ru>
2025-02-16 14:42:25 +0300

    Add user handler

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+	"net/http"
+	"notes-go/internal/user"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	log.Println("Create router")
+	router := httprouter.New()
+
+	log.Println("register user nadler")
+	handler := user.NewHandler()
+	handler.Register(router)
+
+	start(router)
+}
+
+func start(router *httprouter.Router) {
+	log.Println("Start application")
+
+	listener, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		panic(err)
+	}
+
+	server := &http.Server{
+		Handler:      router,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Println("Server is listening port 1234")
+	log.Fatalln(server.Serve(listener))
 }
